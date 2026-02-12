9e6647e3623811acba061a8c567f7f6e0b78e937
ricardontcnhi <ricardontcnhi@gmail.com>
2026-02-12 13:10:37 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
-
-self.addEventListener('install',e=>{e.waitUntil(caches.open('v1').then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('v4').then(c => c.addAll([
+    './', './index.html', './manifest.json'
+  ])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
